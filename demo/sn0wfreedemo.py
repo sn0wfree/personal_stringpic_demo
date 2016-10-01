b4b10b61e7a276e5f8a91400f5b44a230c911be4
sn0wfree <snowfreedom0815@gmail.com>
2016-10-01 02:26:16 +0100

    in——porcess1.0

--- demo/sn0wfreedemo.py
+++ demo/sn0wfreedemo.py
@@ -1,18 +1,16 @@
 #coding=utf-8
 import os,platform,sys
+import Image
 
 
 
-class get_terminal_bound:
 
-    def __init__(self):
-        self.width = None
-        self.height = None
-    def capture_rowsandcolumns(self):
-        rows,columns=os.popen('stty size', 'r').read().split()
-        self.width = int(columns)
-        self.height = int(rows)
-        return (int(rows),int(columns))
+
+def get_terminal_bound():
+    rows,columns=os.popen('stty size', 'r').read().split()
+    width = int(columns)
+    height = int(rows)
+    return (int(rows),int(columns))
 
 class TestPlatform:
     def __init__(self):
@@ -88,30 +86,52 @@ class TestPlatform:
     #    else:
     #        return None
 
-class demo:
+class show_terminal_bound:
     def __init__(self):
-        self.name='demo_video'
+        self.name='show_bound_process'
         rows,columns=os.popen('stty size', 'r').read().split()
         #self.width = 20
         #self.height = 50
         self.width = int(columns)
         self.height = int(rows)
-        self.system='Darwin'
+        self.system= platform.system()#system
 
-    def capture_rowsandcolumns(self, returnout=False):
+
+class pic2symboltext():
+    def __init__(self):
+        self.name='pic2text process'
         rows,columns=os.popen('stty size', 'r').read().split()
         self.width = int(columns)
         self.height = int(rows)
-        if returnout:
-            return (int(rows),int(columns))
-        else:
-            pass
-    def checkOS(self,returnout=False):
-        self.system=platform.system()#system
-        if returnout:
-            return self.system
-        else:
-            pass
+        self.system= platform.system()#system
+
+    def preprocess(self,img_name):
+
+        img = Image.open(img_name)
+        img_w, img_h = img.size
+        #img_m = max(img.size)
+        #windows_w=self.width
+        #windows_h=self.height
+        #img = img.resize((w,h))
+        img = img.convert('L')
+        return img
+
+    def create_symbol_text(img):
+        """ 将已经预处理好的图片根据color生成字符字符串，存放到 pic_str 变量中 """
+        pix = img.load()
+        pic_str = ''
+        width, height = img.size
+        for h in xrange(height):
+                for w in xrange(width):
+                    if pix[w,h]==255:
+                        
+
+                    #pic_str += color[int(pix[w,h]) * 14 /255]
+                pic_str += '\n'
+        return pic_str
+
+
+
     def printdash(self):
         length= self.width
         h=self.height
@@ -124,8 +144,9 @@ class demo:
 
 
 
+
 if __name__=='__main__':
-    v=demo()
+
     a=TestPlatform()
     #print a.platform_system
     v.printdash()
